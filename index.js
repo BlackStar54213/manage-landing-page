f0816703c3506c7e2f89ed8a8adf24c08da284a6
BlackStar54213 <113902603+BlackStar54213@users.noreply.github.com>
2023-09-09 17:59:19 +0100

    let's go

---
+++ index.js
@@ -0,0 +1,10 @@
+function openMenu() {
+    document.getElementById('linktab').style.display = 'flex';
+    document.getElementById('linker').style.display = 'block';
+    document.getElementById('close').style.display = 'block';
+}
+function closeMenu() {
+    document.getElementById('linktab').style.display = 'none';
+    document.getElementById('linker').style.display = 'none';
+    document.getElementById('close').style.display = 'none';
+}
